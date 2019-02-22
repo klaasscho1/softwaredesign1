51bfe10dcfeff7499628399143a75175744906c3
Klaas Schoenmaker <klaas@bunq.com>
2019-02-22 15:59:06 +0100

    Small changes to picker

--- Simbad-Project-Skeleton/src/main/java/softdesign/Robot_picker.java
+++ Simbad-Project-Skeleton/src/main/java/softdesign/Robot_picker.java
@@ -15,7 +15,7 @@ public class Robot_picker extends Agent {
 	private int objectsLeft = 30;
 	
 	public Robot_picker (Vector3d position, String name) {     
-        super(position,name);
+        super(position, name);
     }
     
     public void initBehavior() {
@@ -23,15 +23,33 @@ public class Robot_picker extends Agent {
     }
     
     public void performBehavior() {
+    	// Test if there is an agent near .
+        if (anOtherAgentIsVeryNear()){
+        	System.out.println("Another agent is near. Object?");
+            SimpleAgent agent = getVeryNearAgent();
+            
+            if (agent instanceof CherryAgent){
+                // detach it from the scene graph so it is no more visible.
+                agent.detach();
+                objectsLeft -= 1;
+                System.out.println("Object picked. " + String.valueOf(objectsLeft) + " left.");
+                
+                if(objectsLeft == 0) {
+                	System.out.println("All objects picked!");
+                	// return to base point and stop moving.
+                	moveToStartPosition();
+                	setTranslationalVelocity(0);
+                	setRotationalVelocity(0);
+                }
+            }
+            
+        }
+    	
     	if (collisionDetected()) {
-    		moveToStartPosition();
-        	setTranslationalVelocity(0);
-        	setRotationalVelocity(0);
-        	/*
         	rotateY(180);
 
             setTranslationalVelocity(velocity);         
-            setRotationalVelocity(rotation);*/
+            setRotationalVelocity(rotation);
 
         } else {
         	
@@ -43,27 +61,6 @@ public class Robot_picker extends Agent {
             // frequently change orientation 
             if ((getCounter() % 100)==0) 
                setRotationalVelocity(Math.PI/2 * (0.5 - Math.random()));
-        }
-    	
-        // Test if there is an agent near .
-        if (anOtherAgentIsVeryNear()){
-            SimpleAgent agent = getVeryNearAgent();
-            
-            if (agent instanceof CherryAgent){
-                // detach it from the scene graph so it is no more visible.
-                agent.detach();
-                objectsLeft -= 1;
-                System.out.println("Object picked.");
-                
-                if(objectsLeft == 0) {
-                	System.out.println("All objects picked!");
-                	// return to base point and stop moving.
-                	moveToStartPosition();
-                	setTranslationalVelocity(0);
-                	setRotationalVelocity(0);
-                }
-            }
-            
         }
     }
 }
