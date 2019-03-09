84cba819fb3e7f034f15393b2fd8f76cd575ff0f
srsanos <46721739+srsanos@users.noreply.github.com>
2019-03-09 16:17:44 +0100

    Update CentralStation.java

--- Simbad-Project-Skeleton/src/main/java/softdesign/CentralStation.java
+++ Simbad-Project-Skeleton/src/main/java/softdesign/CentralStation.java
@@ -12,12 +12,8 @@ import javax.vecmath.Vector3d;
 
 public class CentralStation {
 	
-	int numberOfRobots;
-	Point3d coords = new Point3d(0,0,0);
-	
-	
 	CentralStation(){
-		numberOfRobots = 0;
+		
 	}
 	
 	public void initializeRobots(EnvironmentDescription environment, Map<String, ARobotFactory> robots) {
